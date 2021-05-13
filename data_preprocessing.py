0fa7ace2f2708b06b5a6c886646ca245bec0fbd6
KhawlaSeddiki <37381346+KhawlaSeddiki@users.noreply.github.com>
2021-05-13 11:31:55 -0400

    Create data_preprocessing.py

---
+++ data_preprocessing.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as img
+import cv2 as cv
+
+
+def matrixImage(xImg, letter):
+    data = img.imread(xImg)
+    writeindataset(data, letter)
+    return data
+
+def writeindataset(matrice, letter):
+    chemin = "dataImzml/train/"
+    fichier = open(chemin + "dataset.txt", "a") 
+    print(matrice.shape)
+    prob224 = cv.resize(matrice, (224, 56))
+    print(prob224.shape)
+    # data resize
+    prob224_reshape = prob224.reshape((1, 50176))
+    fichier.write(letter)
+    for val in prob224_reshape[0]:
+        fichier.write("," + str(val))  
+    fichier.write("\n")  
+    fichier.close
+
+matrixImage('dataImzml/train/img1.png', '1')
