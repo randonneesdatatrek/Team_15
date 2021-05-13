a3da924dda4361d183328cd8d96065b223ff5e48
KhawlaSeddiki <37381346+KhawlaSeddiki@users.noreply.github.com>
2021-05-13 11:42:39 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -30,3 +30,18 @@ def shuffle() :
     random.shuffle(lines)
     open(chemin+'dataset.txt', 'w').writelines(lines) 
     print("The dataset is ready")
+
+    
+def load_dataset(dataset_file_path):
+    a = np.loadtxt(dataset_file_path, delimiter=',', converters={ 0 : lambda ch : ord(ch)-ord('a') }) #trier le fichier en separant sur chaque ligne chaque élément à l'aide de ','
+    samples, letters = a[:,1:], a[:,0] #letters are the first element on each line, sample are the reste of the line
+    return samples, letters
+
+
+def loaded(chemin, dataset) :
+    [samples, letters] = load_dataset(chemin+dataset)
+    train_ratio = 0.6
+    n_train_samples = int(len(samples) * train_ratio) 
+    x_train, y_train = samples[:n_train_samples], letters[:n_train_samples]
+    x_val, y_val = samples[n_train_samples:], letters[n_train_samples:]
+    return train_ratio, n_train_samples, x_train, x_val, y_train, y_val
